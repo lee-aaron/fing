938134c798fd8751c7b9398f5c14ad8b9512de3c
Aaron Lee <sphinxer34@yahoo.com>
2022-10-07 12:00:28 -0700

    Added sample methods

--- common/src/fs/file.rs
+++ common/src/fs/file.rs
@@ -16,6 +16,36 @@ impl File {
     // Tries to connect to the connection and send content
     pub async fn create(&self, content: String) -> Result<(), SendError> {
         let msg = Bytes::from("create");
-        self.connection.send((msg.clone(), Bytes::new(), Bytes::from(content))).await
+        self.connection
+            .send((msg.clone(), Bytes::new(), Bytes::from(content)))
+            .await
+    }
+
+    pub async fn read(&self, file_name: String) -> Result<(), SendError> {
+        let msg = Bytes::from("read");
+        self.connection
+            .send((msg.clone(), Bytes::new(), Bytes::from(file_name)))
+            .await
+    }
+
+    pub async fn update(&self, file_name: String, content: String) -> Result<(), SendError> {
+        let msg = Bytes::from("update");
+        self.connection
+            .send((msg.clone(), Bytes::new(), Bytes::from(file_name)))
+            .await
+    }
+
+    pub async fn delete(&self, file_name: String) -> Result<(), SendError> {
+        let msg = Bytes::from("delete");
+        self.connection
+            .send((msg.clone(), Bytes::new(), Bytes::from(file_name)))
+            .await
+    }
+
+    pub async fn list(&self, prefix: String) -> Result<(), SendError> {
+        let msg = Bytes::from("list");
+        self.connection
+            .send((msg.clone(), Bytes::new(), Bytes::from(prefix)))
+            .await
     }
 }
